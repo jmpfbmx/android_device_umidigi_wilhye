3b1dd86586a1243615d92d712e1f7eb8b3b1e8d6
Maanush Putcha <p.maanush@gmail.com>
2021-09-18 12:09:42 +0200

    wilhye: init: Update build fingerprint to redfin 2021-03-05
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>
    Change-Id: I9820fa62549eae64d5b5bca9d1c3ab81cab71bb3

--- init/init_wilhye.cpp
+++ init/init_wilhye.cpp
@@ -96,8 +96,8 @@ static void workaround_snet_properties() {
 void vendor_load_properties()
 {
     // fingerprint
-    property_override("ro.build.description", "redfin-user 11 RQ1A.201205.010 6953398 release-keys");
-    property_override_prop("ro.build.fingerprint", "google/redfin/redfin:11/RQ1A.201205.010/6953398:user/release-keys");
+    property_override("ro.build.description", "redfin-user 11 RQ2A.210305.006 7119741 release-keys");
+    property_override_prop("ro.build.fingerprint", "google/redfin/redfin:11/RQ2A.210305.006/7119741:user/release-keys");
 
     // Workaround SafetyNet
     workaround_snet_properties();
