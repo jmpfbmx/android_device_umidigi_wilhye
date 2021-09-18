96853cd96a9262a8883a54286bf869466e13aeb7
Arian <arian.kulmer@web.de>
2021-09-18 11:11:55 +0200

    wilhye: Build libvulkan
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>
    Change-Id: Iabc2d90ceea2007354e621cfac398342967969d2

--- device.mk
+++ device.mk
@@ -28,6 +28,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     Snap
 
+# Display
+PRODUCT_PACKAGES += \
+    libvulkan
+
 # Keylayout
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/ACCDET.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/ACCDET.kl \
