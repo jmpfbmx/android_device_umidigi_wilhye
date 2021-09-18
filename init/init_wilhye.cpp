7c730e34263d2b14cbb81811424a3e9332e6db2f
jmpfbmx <jmpf.bmx@gmail.com>
2021-09-18 12:11:05 +0200

    wilhye: init: Update build fingerprint to redfin 2021-03-05
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>
    Change-Id: I306c77ea90dab755f05a186f594d75563936404d

--- init/init_wilhye.cpp
+++ init/init_wilhye.cpp
@@ -96,8 +96,8 @@ static void workaround_snet_properties() {
 void vendor_load_properties()
 {
     // fingerprint
-    property_override("ro.build.description", "redfin-user 11 RQ2A.210305.006 7119741 release-keys");
-    property_override_prop("ro.build.fingerprint", "google/redfin/redfin:11/RQ2A.210305.006/7119741:user/release-keys");
+    property_override("ro.build.description", "redfin-user 11 RQ2A.210405.005 7181113 release-keys");
+    property_override_prop("ro.build.fingerprint", "google/redfin/redfin:11/RQ2A.210405.005/7181113:user/release-keys");
 
     // Workaround SafetyNet
     workaround_snet_properties();
